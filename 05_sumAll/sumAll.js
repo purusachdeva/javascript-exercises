8787b064a9d4310b8eca4dd3a5e0d6254c030c42
purusachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-12-26 21:20:03 +0530

    complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (
+    start < 0 || end < 0 
+    || !Number.isInteger(start) || !Number.isInteger(end)
+  ) 
+    return 'ERROR'
 
+  if (start > end) {
+    let temp = end
+    end = start
+    start = temp
+  }
+
+  let sum = 0
+
+  while (start <= end) {
+    sum += start
+    start++
+  }
+
+  return sum
 };
 
 // Do not edit below this line
