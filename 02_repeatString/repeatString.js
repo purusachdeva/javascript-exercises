4589fe9f8d90292644ab211d4944c2826d31caca
purusachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-12-26 20:42:36 +0530

    complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+  if (repetitions < 0) 
+    return 'ERROR'
+  let answerString = ''
+  for (let i = 0; i < repetitions; i++) {
+    answerString += string
+  }
+  return answerString
 };
 
 // Do not edit below this line
