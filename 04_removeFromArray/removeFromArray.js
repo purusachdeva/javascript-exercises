0923915ca4c84113e3e0a5da9089afc7db131a33
purusachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-12-26 21:11:38 +0530

    complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  // return arr.filter(val => !args.includes(val))
+
+  let newArray = []
+
+  arr.forEach(val => {
+    if (!args.includes(val)) {
+      newArray.push(val) 
+    }
+  });
+
+  return newArray
 };
 
 // Do not edit below this line
