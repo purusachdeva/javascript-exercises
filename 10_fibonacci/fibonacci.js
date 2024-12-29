55b21199c3d6f0bd7023c84d6d324ccb58cf1901
purusachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-12-29 21:31:07 +0530

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  n = parseInt(n)
+  if (n == 0 || n == 1) return n
+  if (n < 0) return "OOPS"
 
+  return fibonacci(n - 1) + fibonacci(n - 2)
 };
 
 // Do not edit below this line
