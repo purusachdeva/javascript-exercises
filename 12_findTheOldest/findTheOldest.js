9dae57f29289b4c8498976bc73b819f0f7b12780
purusachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-12-29 21:42:23 +0530

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const getAge = (person) => {
+    if (person.yearOfDeath == undefined) {
+      return 2025 - person.yearOfBirth
+    }
 
+    return person.yearOfDeath - person.yearOfBirth
+  }
+
+  return people.reduce((max, current) => {
+    max = getAge(current) > getAge(max) ? current : max
+    return max
+  })
 };
 
 // Do not edit below this line
