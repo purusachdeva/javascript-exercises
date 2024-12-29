9f54400e37aacb9c8d1d58364b565dd9a4294f8c
purusachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-12-29 21:26:48 +0530

    complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,36 @@
-const palindromes = function () {
+function isAlphaNumeric(str) {
+  var code, i, len;
 
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
+      return false;
+    }
+  }
+  return true;
+};
+
+const palindromes = function (string) {
+  let a = 0
+  let b = string.length - 1
+
+  while (a < b) {
+    while(!isAlphaNumeric(string[a])) 
+      a++
+
+    while(!isAlphaNumeric(string[b])) 
+      b--
+
+    if (string[a].toLowerCase() !== string[b].toLowerCase()) 
+      return false
+
+    a++
+    b--
+  }
+
+  return true;
 };
 
 // Do not edit below this line
